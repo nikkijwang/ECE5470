6c1243619bc719df66c7b635e406c2ebbff82f04
nikkijwang <32441461+nikkijwang@users.noreply.github.com>
2019-02-22 20:44:46 -0800

    Updated Gaussian kernel and mnfilter function
    - Added scale parameter to mnfilter function
    - Completed genGaussianKernel function

--- Spatial_Filtering/filtering.py
+++ Spatial_Filtering/filtering.py
@@ -20,13 +20,41 @@ def flipKernel(kernel):
 
 	return res
 
-def mnfilter(image, kernel):
+def scaleValues(img):
+	'''
+	Scales the values of the filtered image
+
+	Arguments:
+	img -- image to be scaled
+
+	Returns:
+	scaled -- scaled image
+	'''
+
+	# Find the minimum and maximum values of the image
+	imgMax = img.max()
+	imgMin = img.min()
+
+	# Calculate the slope
+	slope = 255 / (imgMax - imgMin)
+
+	scaled = np.zeros((img.shape[0], img.shape[1]), dtype = 'int')
+
+	# Scale the image
+	for i in range(img.shape[0]):
+		for j in range(img.shape[1]):
+			scaled[i][j] = ((slope*img[i][j]) - (slope*imgMin)).astype('int')
+
+	return scaled
+
+def mnfilter(image, kernel, scale = 1):
 	'''
 	Filters the input image with the input kernel
 
 	Arguments:
 	image -- input image
 	kernel -- m x n matrix (preferably m = n = odd number)
+	scale -- set to 0 if scaling is not desired, 1 (default) if scaling is desired
 
 	Returns:
 	res -- filtered image
@@ -52,18 +80,47 @@ def mnfilter(image, kernel):
 	padImg = np.pad(image, (a, b), 'edge')
 
 	# Resulting filtered image of the same size as the input
-	res = np.zeros((x, y), dtype = 'int')
+	res = np.zeros((x, y))
 
 	# Filter the image
 	for i in range(1, x):
 		for j in range(1, y):
-			res[i][j] = sum((w[a - s][b - t] * padImg[i - s + a][j - t + b]) 
-				for s in range(a, -a - 1, -1) for t in range(b, -b - 1, -1))
+			res[i][j] = sum((w[a - s][b - t] * padImg[i - s + a][j - t + b]) for s in range(a, -a - 1, -1) for t in range(b, -b - 1, -1))
 
-	return res
+	# Scale if desired and needed
+	if scale:
+		if res.max() > 255 or res.min() < 0:
+			res = scaleValues(res)
 
-def genGaussianKernel():
-	None
+	return res.astype('uint8')
+
+def genGaussianKernel(m, sigma = 1, k = 1):
+	'''
+	Generates an m x m Gaussian kernel with the chosen sigma
+
+	Arguments:
+	m -- size of the kernel, where m is an odd number
+	sigma -- sigma value; default sigma = 1
+	k -- constant; default k = 1
+
+	Returns:
+	kernel -- m x m Gaussian kernel
+	'''
+
+	# Calculate a and b (they're the same in this case)
+	ab = int((m - 1) / 2)
+
+	kernel = np.zeros((m, m))
+
+	# Calculate kernel values
+	for s in range(-ab, ab + 1):
+		for t in range(-ab, ab + 1):
+			e = -(s**2 + t**2) / (2 * (sigma ** 2))
+			kernel[s + ab][t + ab] = k * np.exp(e)
+
+	kernel = kernel / np.sum(kernel)
+
+	return kernel
 
 def LoG():
 	# a) Gaussian of the image, parameters - [sigma, w]
@@ -73,6 +130,7 @@ def LoG():
 def sharpen():
 	None
 
+# For testing purposes
 def genImpulseMatrix(m):
 	mat = np.zeros((m, m), dtype = 'int')
 	mat[int(m/2)][int(m/2)] = 1
@@ -90,15 +148,29 @@ def genKernel(m):
 	return mat
 
 # Input image
-path = "images/img1.jpg"
-img = cv.imread(path)
+path = "images/img2.jpg"
+img = cv.imread(path, 0)
 
-# Kernel used for mnfilter (assumes an m x m matrix where m is odd)
-kernel = np.array([[1, 2, 3],
-	[4, 5, 6],
-	[7, 8, 9]], dtype = 'int')
+# # Kernel used for mnfilter (assumes an m x m matrix where m is odd)
+# kernel = np.array([[1, 2, 3],
+# 				[4, 5, 6],
+# 				[7, 8, 9]], dtype = 'int')
 
 #print(img)
 #print(kernel)
-res = mnfilter(img, kernel)
-print(res)
+# res = mnfilter(img, kernel)
+
+# print("img"); print(img)
+# print("res"); print(res)
+
+# # Show images
+# cv.imshow('Original', img)
+# cv.imshow('Filtered', res)
+
+fil = genGaussianKernel(3)
+print(fil)	
+print(np.sum(fil))
+
+
+cv.waitKey(0)
+cv.destroyAllWindows()
